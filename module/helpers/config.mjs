5bcb7d7f7945a2e60061dfcd1ad4300d64637785
Barry Dewey-Robertson <GMsShadow@hotmail.com>
2025-06-30 11:38:20 +0100

    Named all Fighting Characteristics

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -5,21 +5,23 @@ export const RT87 = {};
  * @type {Object}
  */
 RT87.abilities = {
-  ma: 'RT87.Ability.Ma.long',
-  ws: 'RT87.Ability.Ws.long',
-  dex: 'RT87.Ability.Dex.long',
-  con: 'RT87.Ability.Con.long',
-  int: 'RT87.Ability.Int.long',
-  wis: 'RT87.Ability.Wis.long',
-  cha: 'RT87.Ability.Cha.long',
+  ma: 'RT87.Ability.MA.long',
+  ws: 'RT87.Ability.WS.long',
+  bs: 'RT87.Ability.BS.long',
+  st: 'RT87.Ability.ST.long',
+  to: 'RT87.Ability.TO.long',
+  wo: 'RT87.Ability.WO.long',
+  itv: 'RT87.Ability.Itv.long',
+  att: 'RT87.Ability.Att.long',
 };
 
 RT87.abilityAbbreviations = {
-  ma: 'RT87.Ability.Ma.abbr',
-  ws: 'RT87.Ability.Ws.abbr',
-  dex: 'RT87.Ability.Dex.abbr',
-  con: 'RT87.Ability.Con.abbr',
-  int: 'RT87.Ability.Int.abbr',
-  wis: 'RT87.Ability.Wis.abbr',
-  cha: 'RT87.Ability.Cha.abbr',
+  ma: 'RT87.Ability.MA.abbr',
+  ws: 'RT87.Ability.WS.abbr',
+  bs: 'RT87.Ability.BS.abbr',
+  st: 'RT87.Ability.ST.abbr',
+  to: 'RT87.Ability.TO.abbr',
+  wo: 'RT87.Ability.WO.abbr',
+  itv: 'RT87.Ability.Itv.abbr',
+  att: 'RT87.Ability.Att.abbr',
 };
