9f200578d14e6ca1625bd039bd95e74c9ecf9a39
Barry Dewey-Robertson <GMsShadow@hotmail.com>
2025-07-21 12:10:13 +0100

    Spell power level 1-4 fix

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -212,16 +212,10 @@ export class rt87ActorSheet extends api.HandlebarsApplicationMixin(
     const gear = [];
     const features = [];
     const spells = {
-      0: [],
       1: [],
       2: [],
       3: [],
       4: [],
-      5: [],
-      6: [],
-      7: [],
-      8: [],
-      9: [],
     };
 
     // Iterate through items, allocating to containers
